28edf8b686b09daa74fcb6c348e7afd0191be7f2
Nar der Levonian <narderlevonian@icloud.com>
2023-01-23 07:57:43 +0300

    Begin.

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Icon500px from "./images/500px.svg";
+import IconBandcamp from "./images/Bandcamp.svg";
+import IconDribbble from "./images/Dribbble.svg";
+import IconFacebook from "./images/Facebook.svg";
+import IconGitHub from "./images/GitHub.svg";
+import IconInstagram from "./images/Instagram.svg";
+import IconLinkedIn from "./images/LinkedIn.svg";
+import IconMedium from "./images/Medium.svg";
+import IconTwitter from "./images/Twitter.svg";
+import IconYouTube from "./images/YouTube.svg";
+
+
+const Name = "Nar der Levonian";
+const Description = "Interface Designer and Software Developer";
+const Info = "I am engaged in visionary art (experimental music, photography of a new vision, and (de) generative art), gonzo journalism, and the Romanization of the Russian language.";
+const LinksTitle = "Get in touch with me:";
+const LinksArray = [
+    {
+        Icon: IconInstagram,
+        Link: "http://instagram.com/narderlevonian"
+    },
+    {
+        Icon: IconFacebook,
+        Link: "https://www.facebook.com/profile.php?id=100089061914813"
+    },
+    {
+        Icon: IconTwitter,
+        Link: "http://twitter.com/narderlevonian"
+    },
+    {
+        Icon: IconLinkedIn,
+        Link: "https://www.linkedin.com/in/narderlevoniani"
+    },
+    {
+        Icon: IconMedium,
+        Link: "https://medium.com/@narderlevonian"
+    },
+    {
+        Icon: Icon500px,
+        Link: "http://500px.com/narderlevonian"
+    },
+    {
+        Icon: IconBandcamp,
+        Link: "http://narderlevonian.bandcamp.com"
+    },
+    {
+        Icon: IconYouTube,
+        Link: "http://youtube.com/@narderlevoniani"
+    },
+    {
+        Icon: IconGitHub,
+        Link: "http://github.com/narderlevonian"
+    },
+    {
+        Icon: IconDribbble,
+        Link: "http://dribbble.com/narderlevonian"
+    },
+]
+
+function Links() {
+    return LinksArray.map((item, i) => {
+        return (
+            <a className="Div-LinkItem" href={item.Link} target="_blank">
+                <img src={item.Icon} />
+            </a>
+        )
+    });
+}
+
+function App() {
+    return (
+        <div className="Main">
+            <p className="Paragraph-Name">{Name}</p>
+            <p className="Paragraph-Description">{Description}</p>
+            <p className="Paragraph-Info">{Info}</p>
+            <p className="Paragraph-Links">{LinksTitle}</p>
+            <div className="Div-Links">
+                {Links()}
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,62 @@
+module.exports = [
+    {
+        caption: " ",
+        content: [
+            {
+                caption: "Instagram",
+                description: "zdesj ja vsegda",
+                link: "http://instagram.com/narderlevonian"
+            },
+            {
+                caption: "Facebook",
+                description: "zdesj redko",
+                link: "https://www.facebook.com/narderlevonian"
+            },
+            {
+                caption: "Twitter",
+                description: "zdesj vsäkoje raznoe",
+                link: "https://twitter.com/narderlevonian"
+            },
+            {
+                caption: "Medium",
+                description: "zdesj publikacii",
+                link: "http://medium.com/@narderlevonian"
+            },
+            {
+                caption: "500px",
+                description: "zdesj foto",
+                link: "https://500px.com/narderlevonian"
+            },
+            {
+                caption: "Bandcamp",
+                description: "zdesj muzyka",
+                link: "http://narderlevonian.bandcamp.com"
+            },
+            {
+                caption: "YouTube",
+                description: "zdesj arxiv video",
+                link: "https://www.youtube.com/channel/UCTtdUMHCGRHQgXA324-xzhw"
+            },
+            {
+                caption: "GitHub",
+                description: "zdesj kod",
+                link: "http://github.com/narderlevonian"
+            },
+            {
+                caption: "Dribbble",
+                description: "zdesj dizajn",
+                link: "http://dribbble.com/narderlevonian"
+            }
+        ]
+    },
+    {
+        caption: " ",
+        content: [
+            {
+                caption: "Klükva",
+                description: "zdesj moj latinizator",
+                link: "http://narderlevonian.online/klukva-translator"
+            }
+        ]
+    }
+];
