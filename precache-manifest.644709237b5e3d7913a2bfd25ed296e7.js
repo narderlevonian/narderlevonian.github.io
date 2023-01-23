8fca299f60d1f75ee7706e5129858c339d10d986
Nar der Levonian <narderlevonian@icloud.com>
2023-01-23 08:21:46 +0300

    Updates

---
+++ precache-manifest.644709237b5e3d7913a2bfd25ed296e7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6fe815836980fb27c52",
+    "url": "/static/css/main.45d33f46.chunk.css"
+  },
+  {
+    "revision": "d6fe815836980fb27c52",
+    "url": "/static/js/main.d6fe8158.chunk.js"
+  },
+  {
+    "revision": "2981538dd718ef902032",
+    "url": "/static/js/1.2981538d.chunk.js"
+  },
+  {
+    "revision": "b5eaaaa31c98e598dccb",
+    "url": "/static/js/2.b5eaaaa3.chunk.js"
+  },
+  {
+    "revision": "7855737d81de59457bd8",
+    "url": "/static/js/runtime~main.7855737d.js"
+  },
+  {
+    "revision": "eb76e924ea91a9a6bcfeaa98f23abc7f",
+    "url": "/static/media/500px.eb76e924.svg"
+  },
+  {
+    "revision": "27a9b7fc4420235741d13dc36ebddb89",
+    "url": "/static/media/Bandcamp.27a9b7fc.svg"
+  },
+  {
+    "revision": "02ab4ca831dba192970b0401c85e8494",
+    "url": "/static/media/Dribbble.02ab4ca8.svg"
+  },
+  {
+    "revision": "f3efba5da22b7220a7cf0d1bc84c275b",
+    "url": "/static/media/Facebook.f3efba5d.svg"
+  },
+  {
+    "revision": "59a80e43f8d6b661b7af295236bb5fef",
+    "url": "/static/media/GitHub.59a80e43.svg"
+  },
+  {
+    "revision": "56eb4c4fb97a3407025d43811d69c10b",
+    "url": "/static/media/Instagram.56eb4c4f.svg"
+  },
+  {
+    "revision": "e047b14b7679e41188c0f061218b591f",
+    "url": "/static/media/LinkedIn.e047b14b.svg"
+  },
+  {
+    "revision": "154203353bdaa682e29e888c1d0e80c9",
+    "url": "/static/media/Medium.15420335.svg"
+  },
+  {
+    "revision": "666c26ee827e65a98d53cc1ae3463aed",
+    "url": "/static/media/Twitter.666c26ee.svg"
+  },
+  {
+    "revision": "23b40581f5b6fb0e7bdf85e23887bbdf",
+    "url": "/static/media/YouTube.23b40581.svg"
+  },
+  {
+    "revision": "e83a0dd8fdef0b5f9ff0d58ac07f5dc6",
+    "url": "/index.html"
+  }
+];
